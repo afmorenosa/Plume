e52346b9db4e3ef6ee085c0c39013c35cc5cc4a1
afduquebr <afduquebr@unal.edu.co>
2021-07-28 10:06:20 -0500

    Optical Photon Counter and Secondary Electron Counter Added

--- src/L_TrackingAction.cpp
+++ src/L_TrackingAction.cpp
@@ -13,15 +13,35 @@ _eventAction(evAct), G4UserTrackingAction() { }
 L_TrackingAction::~L_TrackingAction() { }
 
 void L_TrackingAction::PreUserTrackingAction(const G4Track* track) {
+
   G4String VolName = track->GetVolume()->GetLogicalVolume()->GetName();
 
   const G4DynamicParticle *aParticle = track->GetDynamicParticle();
 
-  if (aParticle->GetDefinition()->GetParticleName() != "opticalphoton")
-  return;
-
-  if (VolName == "tablet") {
-      // G4int stationID = atoi(detectorWords[2]);
-      _eventAction->InsertPhotonCreation();
+  if (aParticle->GetDefinition()->GetParticleName() == "opticalphoton"){
+    CountPhotons(track, VolName);
+  } else if (aParticle->GetDefinition()->GetParticleName() == "e-") {
+    CountElectron(track, VolName);
   }
+
+}
+
+void L_TrackingAction::CountPhotons(const G4Track* track, G4String VolName) {
+
+  if (track->GetParentID() == 1 && VolName == "tablet") {
+    _eventAction->InsertPhotonCreation();
+  }
+
+}
+
+void L_TrackingAction::CountElectron(const G4Track* track, G4String VolName) {
+
+  if (track->GetTrackID() != 1) {
+
+    _eventAction->InsertElectronCreation();
+
+    _eventAction->SecondaryElectronEnergy(track->GetTotalEnergy());
+
+  }
+
 }
