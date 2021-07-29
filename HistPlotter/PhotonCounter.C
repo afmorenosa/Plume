d8b9801577029c9b082248b2bc309685b626c3df
afmorenos <afmorenosa@unal.edu.co>
2021-07-28 22:43:09 -0500

    Make plot of the electrons energy

--- HistPlotter/PhotonCounter.C
+++ HistPlotter/PhotonCounter.C
@@ -19,20 +19,28 @@ void PhotonCounter() {
   tree->SetBranchAddress("nPhotReflected", &nPhotReflected);
   tree->SetBranchAddress("secElecEnergy", &secElecEnergy);
 
-  TH1F *hist_photon_counter = new TH1F(
+  TH1I *hist_photon_counter = new TH1I(
     "Photon Counter",
     "Phonton Counter; n Photon; Events",
-    100,
+    200,
     200,
     800
   );
 
-  TH1F *hist_photon_reflected = new TH1F(
+  TH1I *hist_photon_reflected = new TH1I(
     "Photon Reflected",
     "Phonton Reflected; Photons reflected; Events",
-    100,
+    200,
     0,
     200
+  );
+
+  TH1F *hist_electron = new TH1F(
+    "Electron Energy",
+    "Electron Energy; Energy [GeV]; Events",
+    100,
+    0,
+    50
   );
 
   int nentries, nbytes;
@@ -43,11 +51,9 @@ void PhotonCounter() {
     hist_photon_counter->Fill(nPhotCreated);
     hist_photon_reflected->Fill(nPhotReflected);
 
-    std::cout << "A: ";
     for (size_t i = 0; i < secElecEnergy->size(); i++) {
-      std::cout << secElecEnergy->at(i) << '\t';
+      hist_electron->Fill(secElecEnergy->at(i)/100);
     }
-    std::cout << '\n';
   }
 
   hist_photon_counter->SetFillColor(kYellow);
@@ -58,6 +64,12 @@ void PhotonCounter() {
   hist_photon_reflected->SetFillColor(kYellow);
   hist_photon_reflected->Draw();
   canvas->Print("reflected.pdf");
+  canvas->Clear();
+
+  hist_electron->SetFillColor(kYellow);
+  hist_electron->Draw();
+  canvas->SetLogy();
+  canvas->Print("electron.pdf");
   canvas->Clear();
 
 }
