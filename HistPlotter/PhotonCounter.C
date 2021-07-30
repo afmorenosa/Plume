80f4dd669aecea912b32df50a9889b68581044f9
afduquebr <afduquebr@unal.edu.co>
2021-07-29 23:14:34 -0500

    Root Script Updated

--- HistPlotter/PhotonCounter.C
+++ HistPlotter/PhotonCounter.C
@@ -13,26 +13,62 @@ void PhotonCounter() {
 
   Int_t nPhotCreated = -1;
   Int_t nPhotReflected = -1;
+  Int_t nSecondaryPhotCreated = -1;
+  Int_t nElecCreated = -1;
   std::vector<double> *secElecEnergy = {};
 
   tree->SetBranchAddress("nPhotCreated", &nPhotCreated);
   tree->SetBranchAddress("nPhotReflected", &nPhotReflected);
+  tree->SetBranchAddress("nSecondaryPhotCreated", &nSecondaryPhotCreated);
+  tree->SetBranchAddress("nElecCreated", &nElecCreated);
   tree->SetBranchAddress("secElecEnergy", &secElecEnergy);
 
   TH1I *hist_photon_counter = new TH1I(
     "Photon Counter",
-    "Phonton Counter; n Photon; Events",
+    "Photon Counter; n Photon; Events",
+    100,
     200,
+    800
+  );
+
+  TH1I *hist_photon_counter_totals = new TH1I(
+    "Photon Counter Totals",
+    "Photon Counter Totals; n Photon; Events",
+    100,
     200,
     800
   );
 
   TH1I *hist_photon_reflected = new TH1I(
     "Photon Reflected",
-    "Phonton Reflected; Photons reflected; Events",
+    "Photon Reflected; Photons reflected; Events",
     200,
     0,
     200
+  );
+
+  TH1I *hist_secondary_photon_counter = new TH1I(
+    "Secondary Photon Counter",
+    "Secondary Photon Counter; n Photons; Events",
+    100,
+    0,
+    400
+  );
+
+  TH1I *hist_secondary_photon_counter_tail = new TH1I(
+    "Secondary Photon Counter tail",
+    "Secondary Photon Counter tail; n Photons; Events",
+    100,
+    200,
+    800
+  );
+
+  TH1I *hist_electron_counter = new TH1I(
+    "Electron Counter",
+    "Electron Counter; n Electrons; Events",
+    30,
+    0,
+    30
   );
 
   TH1F *hist_electron = new TH1F(
@@ -40,7 +76,7 @@ void PhotonCounter() {
     "Electron Energy; Energy [GeV]; Events",
     100,
     0,
-    50
+    6
   );
 
   int nentries, nbytes;
@@ -51,8 +87,19 @@ void PhotonCounter() {
     hist_photon_counter->Fill(nPhotCreated);
     hist_photon_reflected->Fill(nPhotReflected);
 
+    hist_secondary_photon_counter->Fill(nSecondaryPhotCreated);
+    hist_electron_counter->Fill(nElecCreated);
+
+    hist_photon_counter_totals->Fill(nSecondaryPhotCreated + nPhotCreated);
+
+    if (nSecondaryPhotCreated > 0) {
+      hist_secondary_photon_counter_tail->Fill(
+        nSecondaryPhotCreated + nPhotCreated
+      );
+    }
+
     for (size_t i = 0; i < secElecEnergy->size(); i++) {
-      hist_electron->Fill(secElecEnergy->at(i)/100);
+      hist_electron->Fill(secElecEnergy->at(i)/1000);
     }
   }
 
@@ -61,15 +108,36 @@ void PhotonCounter() {
   canvas->Print("quartz.pdf");
   canvas->Clear();
 
+  hist_photon_counter->SetFillColorAlpha(kYellow, 1.0);
+  hist_photon_counter_totals->SetFillColorAlpha(kRed, 0.5);
+  hist_secondary_photon_counter_tail->SetFillColorAlpha(kCyan, 0.25);
+  hist_photon_counter->Draw();
+  hist_photon_counter_totals->Draw("SAME");
+  hist_secondary_photon_counter_tail->Draw("SAME");
+  canvas->Print("total_photons.pdf");
+  canvas->Clear();
+
   hist_photon_reflected->SetFillColor(kYellow);
   hist_photon_reflected->Draw();
   canvas->Print("reflected.pdf");
   canvas->Clear();
 
+  // Log y plots
+  canvas->SetLogy();
+
   hist_electron->SetFillColor(kYellow);
   hist_electron->Draw();
-  canvas->SetLogy();
   canvas->Print("electron.pdf");
+  canvas->Clear();
+
+  hist_secondary_photon_counter->SetFillColor(kYellow);
+  hist_secondary_photon_counter->Draw();
+  canvas->Print("secondary_photons.pdf");
+  canvas->Clear();
+
+  hist_electron_counter->SetFillColor(kYellow);
+  hist_electron_counter->Draw();
+  canvas->Print("electrons.pdf");
   canvas->Clear();
 
 }
