57bded0eb8c4a50f3309acffad186f11cf6b2122
afduquebr <afduquebr@unal.edu.co>
2021-09-16 23:06:54 -0500

    Root Scripts updated

--- HistPlotter/RadiusDependency.C
+++ HistPlotter/RadiusDependency.C
@@ -1,10 +1,11 @@
+
 void RadiusDependency () {
 
   TCanvas *canvas = new TCanvas(
     "canvas",
     "canvas",
     800,
-    600
+    800
   );
 
   TFile *root_file = TFile::Open("quartz.root");
@@ -15,6 +16,15 @@ void RadiusDependency () {
 
   // Hit Angle
   double hitAngle = 0.0;
+  // Zones
+  Int_t Zone = -1;
+
+  // Counter of Photons in Zone Detector
+  Int_t nPhotonZone0 = -1;
+  Int_t nPhotonZone1 = -1;
+  Int_t nPhotonZone2 = -1;
+  Int_t nPhotonZone3 = -1;
+  Int_t nPhotonZone4 = -1;
 
   // Detected Photons Counter
   Int_t nPhotonDetected = -1;
@@ -27,6 +37,19 @@ void RadiusDependency () {
 
   // Hit Angle
   tree->SetBranchAddress("hitAngle", &hitAngle);
+  // Positions of Primary Electron
+  Double_t nPostPVxPosition = -1.0;
+  Double_t nPostPVyPosition = -1.0;
+
+  // Zones
+  tree->SetBranchAddress("Zone", &Zone);
+
+  // Counter of Photons in Zone Detector
+  tree->SetBranchAddress("nPhotonZone0", &nPhotonZone0);
+  tree->SetBranchAddress("nPhotonZone1", &nPhotonZone1);
+  tree->SetBranchAddress("nPhotonZone2", &nPhotonZone2);
+  tree->SetBranchAddress("nPhotonZone3", &nPhotonZone3);
+  tree->SetBranchAddress("nPhotonZone4", &nPhotonZone4);
 
   // Detected Photons Counter
   tree->SetBranchAddress("nPhotonDetected", &nPhotonDetected);
@@ -36,6 +59,10 @@ void RadiusDependency () {
   tree->SetBranchAddress("nPhotonDetectedPosition", &nPhotonDetectedPosition);
   tree->SetBranchAddress("nPriPhotonDetectedPosition", &nPriPhotonDetectedPosition);
   tree->SetBranchAddress("nSecPhotonDetectedPosition", &nSecPhotonDetectedPosition);
+
+  // Positions of Primary Electron
+  tree->SetBranchAddress("nPostPVxPosition", &nPostPVxPosition);
+  tree->SetBranchAddress("nPostPVyPosition", &nPostPVyPosition);
 
   //--------------------------------------------------------------------//
 
@@ -111,7 +138,7 @@ void RadiusDependency () {
   );
 
   TH1I *hist_secondary_photon_detected_counter_tail = new TH1I(
-    "Secondary Detected Photons Counter tail",
+    "Secondary Photons Detected Counter tail",
     "Secondary Photons Detected Counter tail; n Photons; Events",
     100,
     0,
@@ -122,7 +149,7 @@ void RadiusDependency () {
 
   TH1I *hist_total_photon_detected_position = new TH1I(
     "Positions of Total Detected Photons",
-    "Positions of Total Photons; radius; Events",
+    "Positions of Total Detected Photons; Radius [mm]; Events",
     100,
     0,
     5
@@ -130,7 +157,7 @@ void RadiusDependency () {
 
   TH1I *hist_primary_photon_detected_position = new TH1I(
     "Positions of Primary Detected Photons",
-    "Positions of Primary Photons; radius; Events",
+    "Positions of Primary Detected Photons; Radius [mm]; Events",
     100,
     0,
     5
@@ -138,13 +165,36 @@ void RadiusDependency () {
 
   TH1I *hist_secondary_photon_detected_position = new TH1I(
     "Positions of Secondary Detected Photons",
-    "Positions of Secondary Photons; radius; Events",
+    "Positions of Secondary Detected Photons; Radius [mm]; Events",
     100,
     0,
     5
   );
 
+  TH1F *hist_total_photon_detected_position_normalized = new TH1F(
+    "Positions of Total Detected Photons Normalized",
+    "Positions of Total Detected Photons Normalized; Radius [mm]; Events",
+    100,
+    0,
+    5
+  );
+
+
+  // 2D Positions Histogram of Primary Electron
+
+  TH2D *hist_pos_PVPosition = new TH2D(
+    "Electron Entrance Position",
+    "Electron Entrance Position; X [mm]; Y[mm]; ",
+    100,
+    -5,
+    5,
+    100,
+    -5,
+    5
+  );
+
   //--------------------------------------------------------------------//
+
 
   int nentries, nbytes;
   nentries = (Int_t)tree->GetEntries();
@@ -154,6 +204,7 @@ void RadiusDependency () {
 
     // Hit Angle
     hist_hit_angle->Fill(hitAngle / M_PI * 180);
+    // if (Zone != 4) continue;
 
     // Detected Photons Counter
     hist_total_photon_detected_counter->Fill(nPhotonDetected);
@@ -195,6 +246,23 @@ void RadiusDependency () {
     for (size_t j = 0; j < nSecPhotonDetectedPosition->size(); j++) {
       hist_secondary_photon_detected_position->Fill(
         nSecPhotonDetectedPosition->at(j)
+      );
+    }
+
+    // Positions of Primary Electron
+    hist_pos_PVPosition->Fill(nPostPVxPosition, nPostPVyPosition);
+
+    std::map<Int_t, Int_t> zones_counter;
+
+    zones_counter[0] = nPhotonZone0;
+    zones_counter[1] = nPhotonZone1;
+    zones_counter[2] = nPhotonZone2;
+    zones_counter[3] = nPhotonZone3;
+    zones_counter[4] = nPhotonZone4;
+
+    for (size_t j = 0; j < nPhotonDetectedPosition->size(); j++) {
+      hist_total_photon_detected_position_normalized->Fill(
+        nPhotonDetectedPosition->at(j), 1.0/(zones_counter[Zone]*1.0)
       );
     }
 
@@ -272,7 +340,73 @@ hist_secondary_photon_detected_counter_tail->Draw("SAME");
 canvas->Print("Totals/total_photons_detected_counter_tail.pdf");
 canvas->Clear();
 
-hist_primary_photon_detected_angle_counter->Draw("COLZ");
-hist_primary_photon_detected_angle_counter->SetStats(kFALSE);
+// -------------------------------------------------------------- //
+
+hist_pos_PVPosition->Draw("COLZ");
+hist_pos_PVPosition->SetStats(kFALSE);
+TEllipse *elip1 = new TEllipse(0, 0, std::sqrt(5*1), std::sqrt(5*1));
+TEllipse *elip2 = new TEllipse(0, 0, std::sqrt(5*2), std::sqrt(5*2));
+TEllipse *elip3 = new TEllipse(0, 0, std::sqrt(5*3), std::sqrt(5*3));
+TEllipse *elip4 = new TEllipse(0, 0, std::sqrt(5*4), std::sqrt(5*4));
+TEllipse *elip5 = new TEllipse(0, 0, std::sqrt(5*5), std::sqrt(5*5));
+
+elip1->SetLineWidth(2);
+elip2->SetLineWidth(2);
+elip3->SetLineWidth(2);
+elip4->SetLineWidth(2);
+elip5->SetLineWidth(2);
+
+elip1->SetLineColor(kRed);
+elip2->SetLineColor(kRed);
+elip3->SetLineColor(kRed);
+elip4->SetLineColor(kRed);
+elip5->SetLineColor(kRed);
+
+elip1->SetFillColorAlpha(kRed, 0.0);
+elip2->SetFillColorAlpha(kRed, 0.0);
+elip3->SetFillColorAlpha(kRed, 0.0);
+elip4->SetFillColorAlpha(kRed, 0.0);
+elip5->SetFillColorAlpha(kRed, 0.0);
+
+elip5->Draw("same");
+elip4->Draw("same");
+elip3->Draw("same");
+elip2->Draw("same");
+elip1->Draw("same");
+
+TText *text1 = new TText(0.8, 1.4, "Zone 1");
+TText *text2 = new TText(1.4, 2.1, "Zone 2");
+TText *text3 = new TText(1.9, 2.7, "Zone 3");
+TText *text4 = new TText(2.4, 3.2, "Zone 4");
+TText *text5 = new TText(2.8, 3.6, "Zone 5");
+
+text1->SetTextSize(0.03);
+text2->SetTextSize(0.03);
+text3->SetTextSize(0.03);
+text4->SetTextSize(0.03);
+text5->SetTextSize(0.03);
+
+text1->SetTextAngle(-45.0);
+text2->SetTextAngle(-45.0);
+text3->SetTextAngle(-45.0);
+text4->SetTextAngle(-45.0);
+text5->SetTextAngle(-45.0);
+
+text1->Draw("same");
+text2->Draw("same");
+text3->Draw("same");
+text4->Draw("same");
+text5->Draw("same");
+
+canvas->Print("pos_PVPosition.pdf");
+canvas->Clear();
+
+
+// Test
+
+hist_total_photon_detected_position_normalized->SetFillColor(kYellow);
+hist_total_photon_detected_position_normalized->Draw();
+canvas->Print("Totals/total_photon_detected_position_normalized.pdf");
+canvas->Clear();
 
 }
